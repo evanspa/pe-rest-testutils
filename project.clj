c8aa16d97b36b6927a3a00cf3b9d4ae52f32937c
Paul Evans <evansp2@gmail.com>
2015-09-10 12:05:38 -0400

    Version 0.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-rest-testutils "0.0.7"
+(defproject pe-rest-testutils "0.0.8-SNAPSHOT"
   :description "A Clojure library providing a set of helper functions for unit testing REST applications."
   :url "https://github.com/evanspa/pe-rest-testutils"
   :license {:name "MIT"
